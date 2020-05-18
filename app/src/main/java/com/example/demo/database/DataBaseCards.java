bab42afd001be2da3de9c9ddb430e26c27f5602f
Adilbek Ramazanov <adilbekramazanov@Adilbeks-MacBook-Pro.local>
2020-05-18 21:15:15 +0600

    add new line in DataBaseCards.class

--- app/src/main/java/com/example/demo/database/DataBaseCards.java
+++ app/src/main/java/com/example/demo/database/DataBaseCards.java
@@ -7,4 +7,6 @@ import java.util.ArrayList;
 public class DataBaseCards {
     ArrayList <CardModel> cardModelArrayList = new ArrayList<>();
 
+    ArrayList <CardModel> cardModelArrayList2 = new ArrayList<>();
+
 }
