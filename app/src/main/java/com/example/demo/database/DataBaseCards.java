28c93a8670433e4ec1e5e76777ab5472204bec7b
Adilbek Ramazanov <adilbekramazanov@Adilbeks-MacBook-Pro.local>
2020-05-18 20:46:52 +0600

    create project

---
+++ app/src/main/java/com/example/demo/database/DataBaseCards.java
@@ -0,0 +1,10 @@
+package com.example.demo.database;
+
+import com.example.demo.models.CardModel;
+
+import java.util.ArrayList;
+
+public class DataBaseCards {
+    ArrayList <CardModel> cardModelArrayList = new ArrayList<>();
+
+}
---
+++ app/src/main/java/com/example/demo/models/CardModel.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+public class CardModel {
+    public Integer id;
+    public String type;
+    public String suits;
+    public String name;
+    public String description;
+    public String logoURL;
+
+}
